3a10b9d462713e4a2a4484aac733ccfee474c17a
acdaly <acdaly@andrew.cmu.edu>
2018-07-07 17:37:00 -0400

    readded files to css and javascript folders

---
+++ assets/javascript/test.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.modal-link').on('click', function(){
+        // $('#myModalContent').load('HTML/' +this.id + '.html');
+    })
+    
+});
