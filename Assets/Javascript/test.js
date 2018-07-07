7dbecf65568876b757a2062422643455709d6ecb
acdaly <acdaly@andrew.cmu.edu>
2018-07-07 17:32:32 -0400

    removing Assets folder

--- Assets/Javascript/test.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $('.modal-link').on('click', function(){
-        // $('#myModalContent').load('HTML/' +this.id + '.html');
-    })
-    
-});
