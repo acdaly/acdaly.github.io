ddc37983348056800a37149d417b07297131952d
Ari Daly <aridaly@Aris-MacBook-Pro.local>
2020-05-30 11:11:26 -0400

    changed order of images for phone

--- assets/javascript/detail.js
+++ assets/javascript/detail.js
@@ -14,13 +14,13 @@ function getPageName(){
 $( document ).ready(function() {
 	//fade in home screen
     $('body').removeClass('fade-out');
-    console.log(window.outerWidth);
+    // console.log(window.outerWidth);
 
 	//hide black logo and show white logo on scroll
 	$(document).scroll(function(){
 		var currPos = $(document).scrollTop();
 		if(currPos > 750){
-			console.log(currPos);
+			// console.log(currPos);
 			$(".logo-black").css("opacity", "0");
 			$(".logo-white").css("opacity", "1");
 			$(".detail-title").css("opacity", "1");
